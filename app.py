d25f7aa360ee91c36a46eeadf694f537d578c921
ANA PAULA DA SILVA LIMA <ANA.LIMA@CSTOWER.LOCAL>
2026-06-30 11:12:52 -0300

    Refatoração do layout do login

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+USUARIOS = {
+    "gabriel": "1234",
+    "livia": "1234"
+}
 
 
 @app.route("/")
@@ -11,6 +16,27 @@ def home():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/entrar", methods=["POST"])
+def entrar():
+
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+
+    if usuario in USUARIOS and USUARIOS[usuario] == senha:
+        return redirect(f"/dashboard/{usuario}")
+
+    return render_template("login.html", erro="Senha incorreta")
+
+
+@app.route("/dashboard/<usuario>")
+def dashboard(usuario):
+
+    return render_template(
+        "dashboard.html",
+        usuario=usuario.capitalize()
+    )
 
 
 if __name__ == "__main__":
